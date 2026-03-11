8f6086fd576c9c27439d366dd8e6001f08a3e045
Kush Vaishnani <your_email@gmail.com>
2026-03-11 16:28:51 +0530

    remove line

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import plotly.graph_objects as go
 
 from mt5_helper import connect_mt5, open_trade, close_all
 
-st.warning("⚠️ MT5 trading only works when running locally.")
 
 # ================= PRICE DATA =================
 def get_data():
